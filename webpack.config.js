381706b22d6d794841bb3b874647859336e20a73
Cleiton Luiz <cluiz@mcontigo.com>
2017-08-07 20:42:44 -0300

    Weboack development

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './app/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  config.watch = true;
+  config.devtool = 'source-map';
+}
+
+module.exports = config;
